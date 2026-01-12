6f780999142a86a1a6f3647d3589beb6a18c0b69
Jennifer <612210329@utsalamanca.edu.mx>
2026-01-12 11:29:43 -0600

    endpoint para probar conexion a base de datos

---
+++ lib/mongodb.tsx
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI no está definida");
+}
+
+interface GlobalMongoose {
+  mongoose?: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
+
+const cached = ((global as GlobalMongoose).mongoose) || { conn: null, promise: null };
+
+export async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
